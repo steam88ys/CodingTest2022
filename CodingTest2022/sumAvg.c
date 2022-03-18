5db79ebcc1ccb687b8280a7a5a60167aa7ddbc05
steam88ys <yssteam88@gmail.com>
2022-03-18 12:15:08 +0900

    랜덤함수 사용해서 합계와 평균 구하기

--- CodingTest2022/sumAvg.c
+++ CodingTest2022/sumAvg.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>	// 시간에 따라 랜덤값을 다르게
 
 void main(void)
 {
 	int arr[10], sum = 0;
-	double average = 0;
+	double avg = 0;
+	srand(time(NULL));	// 랜덤값을 다르게
 
 	printf("배열 출력 : ");
 
-	for (int i = 0; i < 10; i++) {
-		arr[i] = rand()%100+1;
-		printf("%d\t", arr[i]);
+	for (int i = 0; i < (sizeof(arr) / sizeof(int)); i++) {
+		arr[i] = (rand()%100)+1;	// 1~100	0~100 => rand()%101
 		sum += arr[i];
+		printf("%d\t", arr[i]);
 	}
 
-	average = (double) sum / (sizeof(arr)/sizeof(int));
+	avg = (double) sum / (sizeof(arr)/sizeof(int));
 
-	printf("\n합계 : %d\n평균 : %0.2f\n", sum, average);
+	printf("\n합계 : %d\n", sum);
+	printf("평균 : %0.2lf\n", avg);
 
 	return 0;
 }
