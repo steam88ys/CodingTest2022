6e2b0f7218f7bd50b2d1fee4fc0e1d5b57a0a430
steam88ys <yssteam88@gmail.com>
2022-03-25 11:16:07 +0900

    코드 수정, 합치기

--- CodingTest2022/getMax.c
+++ CodingTest2022/getMax.c
@@ -3,18 +3,22 @@
 int main(void)
 {
 	int a[10] = {2, 5, 78, 43, -45, 68, 31, 100, 45, 23};
-	int max = a[0];
-	int posi = 0;
+	int max = a[0], min = a[0];
+	int i,  maxposi, minposi;
 	
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		if (max < a[i]) {
-			max = a[i];
-			posi = (i + 1);
+		if (max < a[i]) {	// max값과 i번째 숫자 비교
+			max = a[i];		// max보다 큰 i번째 숫자 선택
+			maxposi = i;	// 위치 기록
+		}
+		if (min > a[i]) {	// min값과 i번째 숫자 비교
+			min = a[i];		// min보다 작은 i번째 숫자 선택
+			minposi = i;	// 위치 기록
 		}
 	}
 
-	printf("최대값 : %d, 위치 : %d", max, posi);
+	printf("최대값 : %d, 위치 : %d \n최소값 : %d, 위치 : %d", max, ++maxposi, min, ++minposi);
 
 	return 0;
 }
--- CodingTest2022/getMin.c
+++ CodingTest2022/getMin.c
@@ -4,17 +4,17 @@ int main(void)
 {
 	int a[10] = { 2, 5, 78, 43, -45, 68, 31, 100, 45, 23 };
 	int min = a[0];
-	int posi = 0;
+	int i, minposi = 0;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (min > a[i]) {
 			min = a[i];
-			posi = (i + 1);
+			minposi = i;
 		}
 	}
 
-	printf("최대값 : %d, 위치 : %d", min, posi);
+	printf("최대값 : %d, 위치 : %d", min, ++minposi);
 
 	return 0;
 }
