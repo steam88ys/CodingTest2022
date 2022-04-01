2217ea0e42181ac2d141046f26f1c68039e5a190
steam88ys <yssteam88@gmail.com>
2022-04-01 12:00:30 +0900

    문자열 복사 (for문 사용)

--- CodingTest2022/StringSize.c
+++ CodingTest2022/StringSize.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	char s1[20] = {"coding test"};
+	char s2[20];
 	int i, j=0;
 	int cnt=0;	// 0, \0
 
@@ -13,13 +14,15 @@ int main(void)
 		}
 		else break;
 	}
-	printf("문자열의 길이 : %d \t(while문 사용)\n", cnt);
+	//printf("문자열의 길이 : %d \t(while문 사용)\n", cnt);
 
 	cnt = 0;
-	for (i = 0; i < 20; i++) 
+	for (i = 0; i <= cnt; i++) 
 	{
+		s2[i] = s1[i];
 		if (s1[i] == '\0') break;
 		cnt++;
 	}
-	printf("문자열의 길이 : %d \t(for문 사용)", cnt);
+	//printf("문자열의 길이 : %d \t(for문 사용)", cnt);
+	printf("복사본 : %s", s2);
 }
