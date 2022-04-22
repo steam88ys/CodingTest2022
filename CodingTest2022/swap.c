17b31ef2b1eeefce6dc6020f7d75e9d1027ddcf9
steam88ys <yssteam88@gmail.com>
2022-04-22 11:47:49 +0900

    포인터 형변환

--- CodingTest2022/swap.c
+++ CodingTest2022/swap.c
@@ -3,20 +3,20 @@
 int main(void) {
 
 	int m = 100, n = 200, dummy;
-	printf("%d %d\n", m, n);
+	printf("%d %d\n", m, n);	// 100 200
 
-	int* p = &m;	// m주소에 n값 넣기
-	dummy = *p;
-	*p = n;
-
-	p = &n;
-	*p = dummy;
+	// 변수 m과 n을 사용하지 않고 두 변수를 서로 교환
+	int* p = &m;	
+	dummy = *p;		// *p == m		dummy = m
+	*p = n;			// m값에 n을 저장
+	p = &n;			// 포인터가 n값 가리킴 
+	*p = dummy;		// n값에 m을 저장
 
 	/*dummy = m;
 	m = n;
 	n = dummy;*/
 
-	printf("%d %d\n", m, n);
+	printf("%d %d\n", m, n);	// 200 100
 	
 	return 0;
 
