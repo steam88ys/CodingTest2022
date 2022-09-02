6a2d8e99598739a56d685257d930dc8ee0d55443
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-09-02 12:49:01 +0900

    장학생 수 구하기

--- codingtest.c
+++ codingtest.c
@@ -2,37 +2,53 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int compare(const void *a, const void *b){
-    return ( *(int*)b - *(int*)a );
+int func_a(int current_grade[], int last_grade[], int rank[], int arr_length, int max_diff_grade) {
+    int count = 0;
+    for (int i = 0; i < arr_length; i++) {
+        if (current_grade[i] >= 80 && rank[i] <= arr_length / 10)
+            count++;
+        else if (current_grade[i] >= 80 && rank[i] == 1)
+            count++;
+        else if (max_diff_grade == current_grade[i] - last_grade[i])
+            count++;
+    }
+    return count;
 }
 
-int func_a(int scores[], int scores_len, int score){
-    for(int rank = 0; rank < scores_len; rank++)
-        if(scores[rank] == score)
-            return rank + 1;
-    return 0;
+int* func_b(int current_grade[], int arr_length) {
+    int *rank = (int*) malloc(sizeof(int) * arr_length);
+    for (int i = 0; i < arr_length; i++) 
+        rank[i] = 1;
+    for (int i = 0; i < arr_length; i++) 
+        for (int j = 0; j < arr_length; j++)
+            if (current_grade[i] < current_grade[j])
+                rank[i]++;
+    return rank;
 }
 
-void func_b(int arr[], int arr_len){
-    qsort(arr, arr_len, sizeof(int), compare);
+int func_c(int current_grade[], int last_grade[], int arr_length) {
+    int max_diff_grade = 1;
+    for (int i = 0; i < arr_length; i++) {
+        if (max_diff_grade < current_grade[i] - last_grade[i])
+            max_diff_grade = current_grade[i] - last_grade[i];
+    }
+    return max_diff_grade;
 }
 
-int func_c(int arr[], int n){
-    return arr[n];
-}
-
-int solution(int scores[], int scores_len, int n) {
-    int score = func_c(scores, n);
-    func_b(scores, scores_len);
-    int answer = func_a(scores, scores_len, score);
+int solution(int current_grade[], int current_grade_len, int last_grade[], int last_grade_len) {
+    int arr_length = current_grade_len;
+    int* rank = func_b(current_grade, arr_length);
+    int max_diff_grade = func_c(current_grade, last_grade, arr_length);
+    int answer = func_a(current_grade, last_grade, rank, arr_length, max_diff_grade);
     return answer;
 }
 
 int main() {
-    int scores[4] ={20, 60, 98, 59};
-    int scores_len = 4;
-    int n = 3;
-    int ret = solution(scores, scores_len, n);
+    int current_grade[6] = {70, 100, 70, 80, 50, 95};
+    int current_grade_len = 6;
+    int last_grade[6] = {35, 65, 80, 50, 20, 60};
+    int last_grade_len = 6;
+    int ret = solution(current_grade, current_grade_len, last_grade, last_grade_len);
 
     printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
