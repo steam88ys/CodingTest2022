d52262f0e422bd39f0a7918bc2382150bec9c08c
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-09-06 08:55:59 +0900

    체조 선수의 점수 구해주기 (구현)

--- codingtest.c
+++ codingtest.c
@@ -2,53 +2,32 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int func_a(int current_grade[], int last_grade[], int rank[], int arr_length, int max_diff_grade) {
-    int count = 0;
-    for (int i = 0; i < arr_length; i++) {
-        if (current_grade[i] >= 80 && rank[i] <= arr_length / 10)
-            count++;
-        else if (current_grade[i] >= 80 && rank[i] == 1)
-            count++;
-        else if (max_diff_grade == current_grade[i] - last_grade[i])
-            count++;
-    }
-    return count;
-}
-
-int* func_b(int current_grade[], int arr_length) {
-    int *rank = (int*) malloc(sizeof(int) * arr_length);
-    for (int i = 0; i < arr_length; i++) 
-        rank[i] = 1;
-    for (int i = 0; i < arr_length; i++) 
-        for (int j = 0; j < arr_length; j++)
-            if (current_grade[i] < current_grade[j])
-                rank[i]++;
-    return rank;
-}
-
-int func_c(int current_grade[], int last_grade[], int arr_length) {
-    int max_diff_grade = 1;
-    for (int i = 0; i < arr_length; i++) {
-        if (max_diff_grade < current_grade[i] - last_grade[i])
-            max_diff_grade = current_grade[i] - last_grade[i];
-    }
-    return max_diff_grade;
-}
-
-int solution(int current_grade[], int current_grade_len, int last_grade[], int last_grade_len) {
-    int arr_length = current_grade_len;
-    int* rank = func_b(current_grade, arr_length);
-    int max_diff_grade = func_c(current_grade, last_grade, arr_length);
-    int answer = func_a(current_grade, last_grade, rank, arr_length, max_diff_grade);
+int solution(int scores[], int scores_len) {
+    int answer = 0;
+    int min=scores[0], max=scores[0], sum=0;
+	
+		for(int i=0; i<scores_len; i++) {
+			if(min>scores[i]) min = scores[i];
+			if(max<scores[i]) max = scores[i];
+			sum+=scores[i];
+		}
+		sum-=max;
+		sum-=min;
+	
+		answer = sum/(scores_len-2);
     return answer;
 }
 
 int main() {
-    int current_grade[6] = {70, 100, 70, 80, 50, 95};
-    int current_grade_len = 6;
-    int last_grade[6] = {35, 65, 80, 50, 20, 60};
-    int last_grade_len = 6;
-    int ret = solution(current_grade, current_grade_len, last_grade, last_grade_len);
-
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+    int scores1[10] = {35, 28, 98, 34, 20, 50, 85, 74, 71, 7};
+    int scores1_len = 10;
+    int ret1 = solution(scores1, scores1_len);
+    
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
+    
+    int scores2[5] = {1, 1, 1, 1, 1};
+    int scores2_len = 5;
+    int ret2 = solution(scores2, scores2_len);
+    
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
 }
