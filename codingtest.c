98f182823c30dd81bf2cdd7a3b893f6931369ab0
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-09-16 12:07:43 +0900

    단어의 오타 수정하기

--- codingtest.c
+++ codingtest.c
@@ -2,32 +2,21 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int scores[], int scores_len) {
-    int answer = 0;
-    int min=scores[0], max=scores[0], sum=0;
-	
-		for(int i=0; i<scores_len; i++) {
-			if(min>scores[i]) min = scores[i];
-			if(max<scores[i]) max = scores[i];
-			sum+=scores[i];
+int solution(char* words[], int words_len, char* word) {
+    int count = 0;
+    for(int i=0; i<words_len; i++) {
+			for(int j=0; j<strlen(words[i]); j++) {
+				if(words[i][j] != word[j]) count++;
+			}
 		}
-		sum-=max;
-		sum-=min;
-	
-		answer = sum/(scores_len-2);
-    return answer;
+    return count;
 }
 
 int main() {
-    int scores1[10] = {35, 28, 98, 34, 20, 50, 85, 74, 71, 7};
-    int scores1_len = 10;
-    int ret1 = solution(scores1, scores1_len);
+    char* words[3] = {"CODE", "COED", "CDEO"};
+    int words_len = 3;
+    char* word = "CODE";
+    int ret = solution(words, words_len, word);
     
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
-    
-    int scores2[5] = {1, 1, 1, 1, 1};
-    int scores2_len = 5;
-    int ret2 = solution(scores2, scores2_len);
-    
-    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
 }
